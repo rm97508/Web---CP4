40ec322da5fef62cff1c526543cbcb46704d797f
rm97508 <112440646+rm97508@users.noreply.github.com>
2023-04-08 03:14:12 -0300

    Update Questão3.js

--- Questão3.js
+++ Questão3.js
@@ -1,2 +1,14 @@
 // Crie uma função tradicional que some A e B e depois faça a divisão por C e apresente agora com o mesmo exemplo crie uma Arrow function para apresentar o mesmo resultado ->
 //Tradicional->
+function somarDividir(a, b, c) {
+  return (a + b) / c;
+}
+const resultado = somarDividir(39, 25, 4);
+console.log(resultado); // Output: 6
+
+//Arrow function->
+const somarDividir = (a, b, c) => (a + b) / c;
+const resultado = somarDividir(39, 25, 4);
+console.log(resultado); // Output: 6
+
+
