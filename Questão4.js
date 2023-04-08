e646baf5b2f2bc918dbef21404642496128d56fe
rm97508 <112440646+rm97508@users.noreply.github.com>
2023-04-08 03:11:10 -0300

    Update Questão4.js

--- Questão4.js
+++ Questão4.js
@@ -1,17 +1,23 @@
-// Criar um programa que calcule a média de 7 notas e exiba uma mensagem indicando se o aluno foi aprovado ou reprovado. Considere que a média mínima para aprovação é 6. Exemplifique e demonstre a saída! ->
-
-const nt1 = parseFloat(prompt("Digite a sua nota 1:"));
-const nt2 = parseFloat(prompt("Digite a sua nota 2:"));
-const nt3 = parseFloat(prompt("Digite a sua nota 3:"));
-const nt4 = parseFloat(prompt("Digite a sua nota 4:"));
-const nt5 = parseFloat(prompt("Digite a sua nota 5:"));
-const nt6 = parseFloat(prompt("Digite a sua nota 6:"));
-const nt7 = parseFloat(prompt("Digite a sua nota 7:"));
-
-const media = (nt1 + nt2 + nt3 + nt4 + nt5 + nt6 + nt7) / 7;
-
-if (media >= 6) {
-  console.log("Parabéns! Você foi aprovado.");
-} else {
-  console.log("Infelizmente você foi reprovado.");
-}
+//Tradicional->
+function contador() {
+    let contador = 0;
+  
+    document.addEventListener("click", function() {
+      contador++;
+      console.log("Você clicou " + contador + " vezes.");
+    });
+  }
+  
+  contador();
+  
+//Arrow function->
+const contador = () => {
+    let contador = 0;
+  
+    document.addEventListener("click", () => {
+      contador++;
+      console.log(`Você clicou ${contador} vezes.`);
+    });
+  }
+  
+  contador();
